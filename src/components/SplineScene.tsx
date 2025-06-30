67e9b14a5fc8bacce6e0fa52b57a3dd8a067fb9f
Yilun_Miko_ Sun <yilunarchi@gmail.com>
2025-06-29 22:07:30 -0700

    Fix Spline Scene Missing Property Error

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -17,6 +17,7 @@ export const SplineScene: React.FC = () => {
           scene="https://prod.spline.design/edOeRvrcuWyGaD41/scene.splinecode"
           style={{ width: '100%', height: '100%' }}
           onError={handleSplineError}
+          scroll-events
         />
       </Suspense>
     </div>
